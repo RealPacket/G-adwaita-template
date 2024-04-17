70e00f43a90bc35b3b71404f75c21a2370b6a9dd
betapacketdlldinknodllforyou <107498533+RealPacket@users.noreply.github.com>
2024-04-17 16:30:11 -0500

    rename `gtk4` to `gtk` and `libadwaita` to `adw` and append list to content

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 /*
-    GAdwaita Template is a Rust GTK 4 + Adwaita template available on GitHub here:
-    <https://github.com/RealPacket/GAdwaita-template>
+	GAdwaita Template is a Rust GTK 4 + Adwaita template available on GitHub here:
+	<https://github.com/RealPacket/GAdwaita-template>
 Copyright (C) 2024  RealPacket / DataModel
 
 This program is free software: you can redistribute it and/or modify
@@ -19,57 +19,53 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 mod config;
 
+use adw::{prelude::*, ActionRow, Application, ApplicationWindow, HeaderBar, WindowTitle};
 use config::{APP_ID, MAIN_WINDOW_TITLE};
-use gtk4::{Application, Box, ListBox, Orientation};
-use libadwaita::{init, prelude::*, ActionRow, ApplicationWindow, HeaderBar, WindowTitle};
+use gtk::{Box, ListBox, Orientation};
 
 fn main() {
-    let application = Application::builder().application_id(APP_ID).build();
+	let application = Application::builder().application_id(APP_ID).build();
 
-    application.connect_startup(|_| {
-        // initialize Adwaita
-        init().unwrap();
-    });
+	application.connect_activate(|app| {
+		// ActionRows are only available in Adwaita
+		let row = ActionRow::builder()
+			.activatable(true)
+			.selectable(false)
+			.title("Click to print \"Activated!\"")
+			.build();
+		row.connect_activated(|_| {
+			eprintln!("Activated!");
+		});
 
-    application.connect_activate(|app| {
-        // ActionRows are only available in Adwaita
-        let row = ActionRow::builder()
-            .activatable(true)
-            .selectable(false)
-            .title("Click to print \"Activated!\"")
-            .build();
-        row.connect_activated(|_| {
-            eprintln!("Activated!");
-        });
+		let list = ListBox::builder()
+			.margin_top(32)
+			.margin_end(32)
+			.margin_bottom(32)
+			.margin_start(32)
+			// the content class makes the list look nicer
+			.css_classes(vec![String::from("content")])
+			.build();
+		list.append(&row);
 
-        let list = ListBox::builder()
-            .margin_top(32)
-            .margin_end(32)
-            .margin_bottom(32)
-            .margin_start(32)
-            // the content class makes the list look nicer
-            .css_classes(vec![String::from("content")])
-            .build();
-        list.append(&row);
+		// Combine the content in a box
+		let content = Box::new(Orientation::Vertical, 0);
+		// Adwaita's ApplicationWindow does not include a HeaderBar
+		content.append(
+			&HeaderBar::builder()
+				.title_widget(&WindowTitle::new(MAIN_WINDOW_TITLE, ""))
+				.build(),
+		);
+		content.append(&list);
+		// TODO(finalize): append your content
 
-        // Combine the content in a box
-        let content = Box::new(Orientation::Vertical, 0);
-        // Adwaita's ApplicationWindow does not include a HeaderBar
-        content.append(
-            &HeaderBar::builder()
-                .title_widget(&WindowTitle::new(MAIN_WINDOW_TITLE, ""))
-                .build(),
-        );
-        // TODO(finalize): append your content
+		let window = ApplicationWindow::builder()
+			.application(app)
+			.default_width(350)
+			// add content to window
+			.content(&content)
+			.build();
+		window.show();
+	});
 
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .default_width(350)
-            // add content to window
-            .content(&content)
-            .build();
-        window.show();
-    });
-
-    application.run();
+	application.run();
 }
